63d3f8032c0d35f8f56950338cc09d0011d3a7b5
dfujimoto <dfujimoto@triumf.ca>
2023-05-25 14:49:15 -0700

    Fixed imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['SDS5034', 'SPD3303', 'SiglentBase', 'RIGOL_DG1032Z']
 
-from .src.SiglentBase import SiglentBase
-from .src.SDS5034 import SDS5034
-from .src.SPD3303 import SPD3303
-from .src.RIGOL_DG1032Z import DG1032Z
+from .SiglentBase import SiglentBase
+from .SDS5034 import SDS5034
+from .SPD3303 import SPD3303
+from .RIGOL_DG1032Z import DG1032Z
