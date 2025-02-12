fcb8bddf5786b7d2a6cb368eb9697227da033dc4
Derek Fujimoto <dfujimoto@triumf.ca>
2025-02-12 14:21:54 -0800

    updated __init__

--- SiglentDevices/__init__.py
+++ SiglentDevices/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ['SDS5034', 'SPD3303', 'SiglentBase', 'RIGOL_DG1032Z']
+__all__ = ['SDS5034', 'SPD3303', 'SiglentBase', 'RIGOL_DG1032Z', 'Keithley_DMM6500']
 
 from .SiglentBase import SiglentBase
 from .SDS5034 import SDS5034
 from .SPD3303 import SPD3303
 from .RIGOL_DG1032Z import DG1032Z
+from .Keithley_DMM6500 import Keithley_DMM6500
